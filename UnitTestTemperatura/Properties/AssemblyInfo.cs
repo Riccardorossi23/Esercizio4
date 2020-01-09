9d86b9c3dd28f670ab8103115b94224fe0fb1d94
Riccardo Rossi <riccardorossi.r2004@outlook.it>
2020-01-09 15:23:04 +0100

    Add project files.

---
+++ UnitTestTemperatura/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTemperatura")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTemperatura")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d85e05d-fdfc-4401-b017-3ba507d4f930")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WPFTemperatura/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFTemperatura
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int i = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int quantità = int.Parse(TxtQuantità.Text);
+            double[] temperatura = new double[quantità];
+            double valorenumerico = double.Parse(TxtNumero.Text);
+
+            for (int i = 0; i < temperatura.Length; i++)
+            {
+                temperatura[i] = valorenumerico;
+            }
+            LblRisultato.Content = Esercizio4.Temperatura.MediaTemperature(temperatura);
+        }
+    }
+}
+
